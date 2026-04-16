a83cacfb5f41e79e357ac19b154d7b10c55c6002
Humberto Junior <astronomi@gmail.com>
2026-04-16 14:25:30 -0300

    Created the CarDetails screen

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+import { CarDTO } from "@/dtos/CarDTO";
+import { getAccessoryIcon } from "@/utils/getAccessoryIcon";
+
+import { Accessory } from "@/components/Accessory";
+import { BackButton } from "@/components/BackButton";
+import { ImageSlider } from "@/components/ImageSlider";
+import { Button } from "@/components/Button";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from "./styles";
+
+interface Params {
+  car: CarDTO;
+}
+
+export function CarDetails() {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { car } = route.params as Params;
+
+  function handleConfirmRental() {
+    navigation.navigate("Scheduling", { car });
+  }
+
+  function handleBackButton() {
+    navigation.goBack();
+  }
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={handleBackButton} />
+      </Header>
+
+      <CarImages>
+        <ImageSlider imagesUrl={car.photos} />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>{car.brand}</Brand>
+            <Name>{car.name}</Name>
+          </Description>
+
+          <Rent>
+            <Period>{car.rent.period}</Period>
+            <Price>R$&nbsp;{car.rent.price}</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          {car.accessories.map((accessory) => (
+            <Accessory
+              key={accessory.type}
+              name={accessory.name}
+              icon={getAccessoryIcon(accessory.type)}
+            />
+          ))}
+        </Accessories>
+
+        <About>{car.about}</About>
+      </Content>
+
+      <Footer>
+        <Button
+          title="Escolher período do aluguel"
+          onPress={handleConfirmRental}
+        />
+      </Footer>
+    </Container>
+  );
+}
