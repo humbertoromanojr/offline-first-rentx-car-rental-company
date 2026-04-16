247030086fa0b8e30cd37ac3de4c801d14e44e68
Humberto Junior <astronomi@gmail.com>
2026-04-16 14:26:41 -0300

    BackButton component created

---
+++ src/components/BackButton/styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native'
+import { TouchableOpacity } from 'react-native'
+
+export const Container = styled(TouchableOpacity)`
+  width: 40px;
+  height: 40px;
+`;
