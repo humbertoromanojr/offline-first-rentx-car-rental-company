1f69c921ae36078f0bb0a93094317f62232519a6
Humberto Junior <astronomi@gmail.com>
2026-04-16 12:07:42 -0300

    Created Car component

---
+++ src/components/Car/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { TouchableOpacityProps } from "react-native";
+
+//import { CarDTO } from '../../dtos/CarDTO'
+//import { getAccessoryIcon } from '../../utils/getAccessoryIcon'
+
+import {
+  Container,
+  Details,
+  Brand,
+  Name,
+  About,
+  Rent,
+  Period,
+  Price,
+  Type,
+  CarImage,
+} from "./styles";
+
+interface Props extends TouchableOpacityProps {
+  //data: CarDTO;
+}
+
+export function Car({ data, ...rest }: Props) {
+  //const MotorIcon = getAccessoryIcon(data.fuel_type);
+
+  return (
+    <Container {...rest}>
+      <Details>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+        <About>
+          <Rent>
+            <Period>{data.rent.period}</Period>
+            <Price>{`RS ${data.rent.price}`}</Price>
+          </Rent>
+          <Type>
+            <MotorIcon />
+          </Type>
+        </About>
+      </Details>
+
+      <CarImage source={{ uri: data.thumbnail }} resizeMode="contain" />
+    </Container>
+  );
+}
