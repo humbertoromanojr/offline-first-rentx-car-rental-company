16cdefe6294658f301d5e9193e29a74ed2af05aa
Humberto Junior <astronomi@gmail.com>
2026-04-18 15:52:38 -0300

    Updating the SchedulingComplete screen

--- src/screens/SchedulingComplete/index.tsx
+++ src/screens/SchedulingComplete/index.tsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import { StatusBar, useWindowDimensions } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import React from "react";
+import { StatusBar, useWindowDimensions } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-import { ConfirmButton } from '../../components/ConfirmButton'
+import { ConfirmButton } from "@/components/ConfirmButton";
 
-import { Container, Content, Title, Message, Footer } from './styles'
+import { Container, Content, Title, Message, Footer } from "./styles";
 
-import LogoSvg from '../../assets/logo_background_gray.svg'
-import DoneSvg from '../../assets/done.svg'
+import LogoSvg from "@/assets/logo_background_gray.svg";
+import DoneSvg from "@/assets/done.svg";
 
-export function SchedulingComplete(){
-  const { width } = useWindowDimensions()
-  const navigation = useNavigation()
+export function SchedulingComplete() {
+  const { width } = useWindowDimensions();
+  const navigation = useNavigation();
 
   function handleConfirmRental() {
-    navigation.navigate('Home');
+    navigation.navigate("Home");
   }
 
   return (
     <Container>
-      <StatusBar 
-        barStyle="light-content" 
-        backgroundColor="transparent" 
-        translucent 
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
       />
       <LogoSvg width={width} />
-      
+
       <Content>
         <DoneSvg width={80} height={80} />
         <Title>Carro alugado!</Title>
 
         <Message>
-          Agora você só precisa ir {'\n'}
-          até a concessionária da RENTX {'\n'}
+          Agora você só precisa ir {"\n"}
+          até a concessionária da RENTX {"\n"}
           pegar o seu automóvel.
         </Message>
       </Content>
@@ -41,5 +41,5 @@ export function SchedulingComplete(){
         <ConfirmButton title="OK" onPress={handleConfirmRental} />
       </Footer>
     </Container>
-  )
+  );
 }
