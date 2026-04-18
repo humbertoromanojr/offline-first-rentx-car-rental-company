dbf7504c9137c655341bb4cdd29d128de547ba9f
Humberto Junior <astronomi@gmail.com>
2026-04-18 15:42:07 -0300

    Adjusting the Accessory component's style

--- src/components/Accessory/styles.tsx
+++ src/components/Accessory/styles.tsx
@@ -1,5 +1,5 @@
-import { RFValue } from 'react-native-responsive-fontsize';
-import styled from 'styled-components/native'
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
 
 export const Container = styled.View`
   justify-content: center;
@@ -11,11 +11,11 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.background_primary};
 
   padding: 16px;
-  margin-bottom: 8px
+  margin-bottom: 8px;
 `;
 
 export const Name = styled.Text`
   font-family: ${({ theme }) => theme.fonts.primary_500};
-  color: ${({ theme }) => theme.colors.text};
+  color: ${({ theme }) => theme.colors.text_detail};
   font-size: ${RFValue(13)}px;
 `;
