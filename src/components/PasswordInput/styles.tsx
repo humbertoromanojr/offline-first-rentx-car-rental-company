fb981afc917547f01d54f60c1cba84015e5da202
Humberto Junior <astronomi@gmail.com>
2026-04-21 10:51:48 -0300

    Created the PasswordInput component and its style

---
+++ src/components/PasswordInput/styles.tsx
@@ -0,0 +1,29 @@
+import { TextInput } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex-direction: row;
+`;
+
+export const IconContainer = styled.View`
+  height: 56px;
+  width: 55px;
+  justify-content: center;
+  align-items: center;
+
+  margin-right: 2px;
+
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+`;
+
+export const InputText = styled(TextInput)`
+  flex: 1;
+
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(16)}px;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+  color: ${({ theme }) => theme.colors.text_detail};
+
+  padding: 0 23px;
+`;
