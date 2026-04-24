b1f052cc19fc053160303afe85fa2dd2f0686f2e
Humberto Junior <astronomi@gmail.com>
2026-04-24 16:37:33 -0300

    Created the SignUpFirstStep screen

---
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import {
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
+import * as Yup from "yup";
+
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { PasswordInput } from "@/components/PasswordInput";
+
+import { useTheme } from "styled-components/native";
+import { Container, Header, Title, SubTitle, Form, Footer } from "./styles";
+
+export function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const theme = useTheme();
+
+  async function handleSigUp() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("Email obrigatório")
+          .email("Digite um email válido"),
+        password: Yup.string().required("Senha obrigatório"),
+      });
+
+      await schema.validate({ email, password });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert("Ops: ", error.message);
+      } else {
+        Alert.alert("Erro na autenticação: ", "Verifique as credenciais");
+      }
+    }
+  }
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <Header>
+            <Title>Estamos {"\n"}quase lá.</Title>
+            <SubTitle>
+              Faça seu login para começar{"\n"}uma experiência incrível.
+            </SubTitle>
+          </Header>
+
+          <Form>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              placeholderTextColor={theme.colors.text_detail}
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={setEmail}
+              value={email}
+            />
+
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              secureTextEntry
+              placeholderTextColor={theme.colors.text_detail}
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={setPassword}
+              value={password}
+            />
+          </Form>
+
+          <Footer>
+            <Button
+              title="Login"
+              onPress={handleSigUp}
+              enabled={true}
+              loading={false}
+            />
+
+            <Button
+              title="Criar conta gratuita"
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+              color={theme.colors.background_secondary}
+              light
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
