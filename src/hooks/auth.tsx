daf7dfe8ce9c2cd9613253e582bd7cbde5608f1e
Humberto Junior <astronomi@gmail.com>
2026-04-27 16:52:17 -0300

    Updating the Auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,1 +1,59 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, ReactNode } from "react";
+
+import { api } from "@/services/api";
+
+interface User {
+  id: string;
+  email: string;
+  name: string;
+  driver_license: string;
+  avatar: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: User;
+  signIn: (credentials: SignInCredentials) => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [data, setData] = useState<AuthState>({} as AuthState);
+
+  async function signIn({ email, password }: SignInCredentials) {
+    const resp = await api.post("/sessions", {
+      email,
+      password,
+    });
+
+    console.log("===== RESP: ", resp.data);
+  }
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { useAuth, AuthProvider };
