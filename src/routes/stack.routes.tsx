c5b0f7961c2781cf9ba584f5b45154dc3a071570
Humberto Junior <astronomi@gmail.com>
2026-04-27 10:31:08 -0300

    Adding a new route

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,17 +8,20 @@ import { CarDetails } from "../screens/CarDetails";
 import { MyCars } from "../screens/MyCars";
 import { SignIn } from "../screens/SignIn";
 import { SignUpFirstStep } from "../screens/SignUp/SignUpFirstStep";
+import { SignUpSecondStep } from "../screens/SignUp/SignUpSecondStep";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function StackRoutes() {
   return (
     <Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
+      <Screen name="Home" component={Home} />
+
       <Screen name="SignIn" component={SignIn} />
 
       <Screen name="SignUpFirstStep" component={SignUpFirstStep} />
 
-      <Screen name="Home" component={Home} />
+      <Screen name="SignUpSecondStep" component={SignUpSecondStep} />
 
       <Screen name="Scheduling" component={Scheduling} />
 
