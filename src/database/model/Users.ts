9a5d7aba1068cefc3258ee6fcf2522f6f3c19957
Humberto Junior <astronomi@gmail.com>
2026-04-28 17:27:09 -0300

    Finished User model file

--- src/database/model/Users.ts
+++ src/database/model/Users.ts
@@ -5,7 +5,22 @@ class User extends Model {
   static table = "users";
 
   @field("user_id")
-  user_id: string;
+  user_id!: string;
+
+  @field("name")
+  name!: string;
+
+  @field("email")
+  email!: string;
+
+  @field("driver_license")
+  driver_license!: string;
+
+  @field("avatar")
+  avatar!: string;
+
+  @field("token")
+  token!: string;
 }
 
 export { User };
