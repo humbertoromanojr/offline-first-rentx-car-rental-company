f5c37f8afd831afa771b51dfac15e7def8f75e58
Humberto Junior <astronomi@gmail.com>
2026-04-28 11:54:18 -0300

    Customizing tab buttons

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,6 +1,10 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Platform } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { AppStackRoutes } from "./app.stack.routes";
+
+import { useTheme } from "styled-components/native";
 
 import { Home } from "../screens/Home";
 import { MyCars } from "../screens/MyCars";
@@ -9,13 +13,64 @@ import { Profile } from "../screens/Profile";
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppTabRoutes() {
+  const theme = useTheme();
+
   return (
-    <Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Screen name="Home" component={AppStackRoutes} />
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: theme.colors.main,
+        tabBarInactiveTintColor: theme.colors.text_detail,
+        tabBarStyle: {
+          paddingVertical: Platform.OS ? 20 : 0,
+          height: 78,
+          backgroundColor: theme.colors.background_primary,
+        },
+      }}
+    >
+      <Screen
+        name="Home"
+        component={AppStackRoutes}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons
+              name="home"
+              size={32}
+              color={color ? color : theme.colors.text}
+            />
+          ),
+        }}
+      />
 
-      <Screen name="Profile" component={Profile} />
+      <Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons
+              name="person"
+              size={32}
+              color={color ? color : theme.colors.text}
+            />
+          ),
+        }}
+      />
 
-      <Screen name="MyCars" component={MyCars} />
+      <Screen
+        name="MyCars"
+        component={MyCars}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons
+              name="directions-car"
+              size={32}
+              color={color ? color : theme.colors.text}
+            />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
