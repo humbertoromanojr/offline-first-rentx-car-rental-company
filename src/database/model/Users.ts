b910a3ac2b4be9566a5f68ede9785ae2d9e554bc
Humberto Junior <astronomi@gmail.com>
2026-04-28 17:20:30 -0300

    Created User model file

---
+++ src/database/model/Users.ts
@@ -0,0 +1,11 @@
+import { Model } from "@nozbe/watermelondb";
+import { field } from "@nozbe/watermelondb/decorators";
+
+class User extends Model {
+  static table = "users";
+
+  @field("user_id")
+  user_id: string;
+}
+
+export { User };
