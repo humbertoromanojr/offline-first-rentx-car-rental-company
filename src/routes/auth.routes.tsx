779575fddefb1c94961d82e38f19b5b915aa7f94
Humberto Junior <astronomi@gmail.com>
2026-04-28 10:15:04 -0300

    Created AuthRoutes route

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,22 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Confirmation } from "../screens/Confirmation";
+import { SignIn } from "../screens/SignIn";
+import { SignUpFirstStep } from "../screens/SignUp/SignUpFirstStep";
+import { SignUpSecondStep } from "../screens/SignUp/SignUpSecondStep";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AuthRoutes() {
+  return (
+    <Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
+      <Screen name="SignIn" component={SignIn} />
+
+      <Screen name="SignUpFirstStep" component={SignUpFirstStep} />
+
+      <Screen name="SignUpSecondStep" component={SignUpSecondStep} />
+
+      <Screen name="Confirmation" component={Confirmation} />
+    </Navigator>
+  );
+}
