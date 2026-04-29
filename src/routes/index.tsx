1709251b7c8e91a12f86d5e50332351b65f69ab7
Humberto Junior <astronomi@gmail.com>
2026-04-29 10:48:43 -0300

    Small adjustment

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ export function Routes() {
 
   return (
     <NavigationContainer>
-      {user ? <AuthRoutes /> : <AppTabRoutes />}
+      {user.id ? <AppTabRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
